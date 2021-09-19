29e90b5b9afcd1befa8aa8ce14203c597a6808f6
Teguh Sutanto <teguh@dinamika.ac.id>
2021-09-19 22:05:05 +0700

    Update Mobil.java
    Menambahkan Attribute dan operation

--- pert2/Mobil.java
+++ pert2/Mobil.java
@@ -1,2 +1,16 @@
 public class Mobil{
+	String colour;
+	double enginePower;
+	boolean isConvertible;
+	boolean isDirectionForwards;
+	boolean parkingBrakeOn;
+	boolean isMoving;
+	boolean engineStarted;
+	public void pressAccelerator(){
+		
+	}
+	public void pressStartButton()
+	{
+		
+	}
 }
