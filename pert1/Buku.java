a720277598430c7ac322c73fbc267d844cfda368
Teguh Sutanto <teguh@dinamika.ac.id>
2021-09-20 12:04:14 +0700

    Update Buku.java
    Mengganti attribute dan operation

--- pert1/Buku.java
+++ pert1/Buku.java
@@ -1,10 +1,34 @@
 public class Buku{
 	String judul;
-	String penulis;
+	String author;
 	int tahun;
 	public Buku(){
 		this.judul="Java Programming";
-		this.penulis="Agung Sedayu";
+		this.author="Agung Sedayu";
 		this.tahun=2021;
 	}
+	public String getJudul() {
+		return judul;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public int getTahun() {
+		return tahun;
+	}
+
+	public void setJudul(String judul) {
+            this.judul=judul;
+	}
+
+	public void setAuthor(String author) {
+            this.author=author;
+	}
+
+	public void setTahun(int tahun) {
+            this.tahun=tahun;
+	}
+
 }
